fb52ceecc1b536663dfaff01b99a45264bb5e260
Denneson <Zenneson@users.noreply.github.com>
2022-12-05 19:20:49 -0500

    Added MapID and the map loads!!!

--- comps/mymap.js
+++ comps/mymap.js
@@ -1,23 +1,19 @@
 import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export default function Mymap() {
-  const { isLoaded } = useLoadScript({
-    // googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    googleMapsApiKey: "AIzaSyAeYHu9_AGQDASLgQ8xiZ_Hd4GvwhOtdQk",
-  });
-
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
-}
-
-function Map() {
   return (
-    <GoogleMap
-      zoom={4.7}
-      center={{ lat: 40, lng: -88 }}
-      mapContainerClassName="mapContainer"
-      mapId="d9abba45cff72fe"
-    ></GoogleMap>
+    <LoadScript
+      googleMapsApiKey="AIzaSyAeYHu9_AGQDASLgQ8xiZ_Hd4GvwhOtdQk"
+      version="beta"
+    >
+      <GoogleMap
+        id="mymap"
+        zoom={4.7}
+        center={{ lat: 40, lng: -88 }}
+        mapContainerClassName="mapContainer"
+        options={{ disableDefaultUI: true, mapId: "d9abba45cff72fe" }}
+      ></GoogleMap>
+    </LoadScript>
   );
 }
