946fb90520a1ce5e4fe3ed5883a50736c94ce87e
Denneson <Zenneson@users.noreply.github.com>
2022-12-06 22:16:34 -0500

    Mantime AppShell and Header added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import {
+  AppShell,
+  Header,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from "@mantine/core";
 import Mymap from "../comps/mymap";
 
 export default function Home() {
-  return <Mymap />;
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <AppShell
+      padding="none"
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            {/* Mobile Menu Burger */}
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>TouraSSist</Text>
+          </div>
+        </Header>
+      }
+    >
+      <Mymap />
+    </AppShell>
+  );
 }
