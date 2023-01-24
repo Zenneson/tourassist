3edff58580d0161e5b221230f04c13d9f25dcadb
Denneson <Zenneson@users.noreply.github.com>
2023-01-23 20:44:46 -0500

    NavBar made

---
+++ comps/navBar.js
@@ -0,0 +1,173 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { getAuth, signOut } from "firebase/auth";
+import {
+  IconQuestionMark,
+  IconLogin,
+  IconLogout,
+  IconSearch,
+} from "@tabler/icons";
+import {
+  Avatar,
+  Header,
+  Button,
+  Image,
+  Group,
+  Tooltip,
+  Popover,
+} from "@mantine/core";
+import Sidebar from "../comps/sidebar";
+import {
+  visibleState,
+  searchOpenedState,
+  loginOpenedState,
+  infoOpenedState,
+} from "../libs/atoms";
+
+export default function NavBar() {
+  const [infoOpened, setInfoOpened] = useRecoilState(infoOpenedState);
+  const [searchOpened, setSearchOpened] = useRecoilState(searchOpenedState);
+  const [visible, setVisible] = useRecoilState(visibleState);
+  const [loginOpened, setLoginOpened] = useRecoilState(loginOpenedState);
+  const [logoutOpeened, setLogoutOpeened] = useState(false);
+
+  const router = useRouter();
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  return (
+    <>
+      <Header
+        zIndex={120}
+        hidden={!visible || infoOpened || searchOpened || loginOpened}
+        sx={{
+          display: "flex",
+          padding: "15px 25px",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Image
+          width={"auto"}
+          height={50}
+          src={"img/blogo.png"}
+          alt="TouraSSist_logo"
+          withPlaceholder
+        />
+        <Group>
+          {user && (
+            <Button
+              variant="subtle"
+              radius="xl"
+              pl={0}
+              pr={12}
+              size="xs"
+              component="a"
+              href="/profile"
+            >
+              <Avatar
+                size={25}
+                src={user?.providerData[0].photoURL}
+                radius="xl"
+                mr={7}
+              />
+              {user?.providerData[0].email}
+            </Button>
+          )}
+          <Group
+            spacing={0}
+            sx={{
+              backgroundColor: "rgba(0,0,0,0.2)",
+              borderRadius: "50px",
+            }}
+          >
+            <Tooltip
+              label="Search Trips"
+              position="bottom"
+              openDelay={800}
+              withArrow
+            >
+              <Button
+                onClick={() => setSearchOpened(true)}
+                variant="subtle"
+                radius="xl"
+                p={10}
+              >
+                <IconSearch size={17} />
+              </Button>
+            </Tooltip>
+
+            <Tooltip
+              label="TourAssist?"
+              position="bottom"
+              openDelay={800}
+              withArrow
+            >
+              <Button
+                onClick={() => setInfoOpened(true)}
+                variant="subtle"
+                radius="xl"
+                p={10}
+              >
+                <IconQuestionMark size={17} />
+              </Button>
+            </Tooltip>
+            <Popover
+              width="auto"
+              position="left"
+              withArrow
+              shadow="md"
+              opened={logoutOpeened}
+              onChange={setLogoutOpeened}
+            >
+              <Tooltip
+                label="Login"
+                position="bottom"
+                openDelay={800}
+                withArrow
+              >
+                <Popover.Target>
+                  <Button
+                    onClick={() => {
+                      if (!user) setLoginOpened(true);
+                      if (user) setLogoutOpeened((o) => !o);
+                    }}
+                    variant="subtle"
+                    radius="xl"
+                    p={10}
+                  >
+                    {user ? <IconLogout size={17} /> : <IconLogin size={17} />}
+                  </Button>
+                </Popover.Target>
+              </Tooltip>
+              <Popover.Dropdown p={0}>
+                <Button
+                  size="xs"
+                  fw={700}
+                  uppercase={true}
+                  variant="default"
+                  sx={{ color: "rgba(255,255,255,0.3)" }}
+                  leftIcon={<IconLogout size={15} />}
+                  onClick={function () {
+                    signOut(auth)
+                      .then(() => {
+                        localStorage.removeItem("user");
+                        router.reload();
+                      })
+                      .catch((error) => {
+                        console.log(error);
+                      });
+                  }}
+                >
+                  Logout
+                </Button>
+              </Popover.Dropdown>
+            </Popover>
+          </Group>
+        </Group>
+        {visible && !infoOpened && !searchOpened && !loginOpened && <Sidebar />}
+      </Header>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,22 @@
 import { useState } from "react";
-import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
-import { getAuth, signOut } from "firebase/auth";
-import {
-  IconQuestionMark,
-  IconLogin,
-  IconLogout,
-  IconSearch,
-  IconList,
-} from "@tabler/icons";
-import {
-  Avatar,
-  Box,
-  Badge,
-  AppShell,
-  Header,
-  Button,
-  Image,
-  Group,
-  Modal,
-  TextInput,
-  Tooltip,
-  Popover,
-} from "@mantine/core";
+import { IconSearch, IconList } from "@tabler/icons";
+import { Box, AppShell, Button, Modal, TextInput } from "@mantine/core";
 import Intro from "../comps/intro";
-import Sidebar from "../comps/sidebar";
 import Mymap from "../comps/Mymap";
 import InfoModal from "../comps/infoModal";
 import LoginModal from "../comps/loginModal";
+import NavBar from "../comps/navBar";
 import {
-  visibleState,
   listOpenedState,
   searchOpenedState,
-  loginOpenedState,
-  infoOpenedState,
   placeListState,
 } from "../libs/atoms";
 
 export default function Home() {
-  const [infoOpened, setInfoOpened] = useRecoilState(infoOpenedState);
   const [searchOpened, setSearchOpened] = useRecoilState(searchOpenedState);
   const [listOpened, setListOpened] = useRecoilState(listOpenedState);
-  const [visible, setVisible] = useRecoilState(visibleState);
-  const [loginOpened, setLoginOpened] = useRecoilState(loginOpenedState);
   const [places, setPlaces] = useRecoilState(placeListState);
-  const [logoutOpeened, setLogoutOpeened] = useState(false);
-
-  const router = useRouter();
-  const auth = getAuth();
-  const user = auth.currentUser;
 
   return (
     <div>
@@ -89,147 +57,7 @@ export default function Home() {
           position: "relative",
         }}
       >
-        <AppShell
-          padding="none"
-          header={
-            <Header
-              zIndex={120}
-              hidden={!visible || infoOpened || searchOpened || loginOpened}
-              sx={{
-                display: "flex",
-                padding: "15px 25px",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <Image
-                width={"auto"}
-                height={50}
-                src={"img/blogo.png"}
-                alt="TouraSSist_logo"
-                withPlaceholder
-              />
-              <Group>
-                {user && (
-                  <Button
-                    variant="subtle"
-                    radius="xl"
-                    pl={0}
-                    pr={12}
-                    size="xs"
-                    component="a"
-                    href="/profile"
-                  >
-                    <Avatar
-                      size={25}
-                      src={user?.providerData[0].photoURL}
-                      radius="xl"
-                      mr={7}
-                    />
-                    {user?.providerData[0].email}
-                  </Button>
-                )}
-                <Group
-                  spacing={0}
-                  sx={{
-                    backgroundColor: "rgba(0,0,0,0.2)",
-                    borderRadius: "50px",
-                  }}
-                >
-                  <Tooltip
-                    label="Search Trips"
-                    position="bottom"
-                    openDelay={800}
-                    withArrow
-                  >
-                    <Button
-                      onClick={() => setSearchOpened(true)}
-                      variant="subtle"
-                      radius="xl"
-                      p={10}
-                    >
-                      <IconSearch size={17} />
-                    </Button>
-                  </Tooltip>
-
-                  <Tooltip
-                    label="TourAssist?"
-                    position="bottom"
-                    openDelay={800}
-                    withArrow
-                  >
-                    <Button
-                      onClick={() => setInfoOpened(true)}
-                      variant="subtle"
-                      radius="xl"
-                      p={10}
-                    >
-                      <IconQuestionMark size={17} />
-                    </Button>
-                  </Tooltip>
-                  <Popover
-                    width="auto"
-                    position="left"
-                    withArrow
-                    shadow="md"
-                    opened={logoutOpeened}
-                    onChange={setLogoutOpeened}
-                  >
-                    <Tooltip
-                      label="Login"
-                      position="bottom"
-                      openDelay={800}
-                      withArrow
-                    >
-                      <Popover.Target>
-                        <Button
-                          onClick={() => {
-                            if (!user) setLoginOpened(true);
-                            if (user) setLogoutOpeened((o) => !o);
-                          }}
-                          variant="subtle"
-                          radius="xl"
-                          p={10}
-                        >
-                          {user ? (
-                            <IconLogout size={17} />
-                          ) : (
-                            <IconLogin size={17} />
-                          )}
-                        </Button>
-                      </Popover.Target>
-                    </Tooltip>
-                    <Popover.Dropdown p={0}>
-                      <Button
-                        size="xs"
-                        fw={700}
-                        uppercase={true}
-                        variant="default"
-                        sx={{ color: "rgba(255,255,255,0.3)" }}
-                        leftIcon={<IconLogout size={15} />}
-                        onClick={function () {
-                          signOut(auth)
-                            .then(() => {
-                              localStorage.removeItem("user");
-                              router.reload();
-                            })
-                            .catch((error) => {
-                              console.log(error);
-                            });
-                        }}
-                      >
-                        Logout
-                      </Button>
-                    </Popover.Dropdown>
-                  </Popover>
-                </Group>
-              </Group>
-              {visible && !infoOpened && !searchOpened && !loginOpened && (
-                <Sidebar />
-              )}
-            </Header>
-          }
-        >
+        <AppShell padding="none" header={<NavBar />}>
           {places.length >= 1 && !listOpened && (
             <Button
               onClick={() => setListOpened(true)}
